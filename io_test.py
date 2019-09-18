89a565aa536819189d64df034c87eb21040c7ee0
David Zhu <david.zhu666@gmail.com>
2019-09-18 14:55:18 +0800

    no message

--- io_test.py
+++ io_test.py
@@ -1,9 +1,9 @@
 # -*- coding:utf-8 -*-
 
-# str = input("Please key in:")
-# print("What you input is:" + str)
+str = input("Please key in:")
+print("What you input is:" + str)
 
-# print ("Python is a great programming language..")
+print ("Python is a great programming language..")
 
 f = open("foo.txt", "w")
 f.write("Python is a great language...")
